008dcdb51d7c14be2742afb98a459d287d68cc3d
Shade2231S <matthewmcculloch149@gmail.com>
2025-11-17 18:34:18 -0400

    Angler Timeline
    timeline resets when ends, game object disable's when ends.

--- Assets/Scripts/TriggerTimeline.cs
+++ Assets/Scripts/TriggerTimeline.cs
@@ -1,9 +1,34 @@
+using System.Collections;
 using UnityEngine;
-
+using UnityEngine.Playables;
+using UnityEngine.Playables;
 public class TriggeTimeline : MonoBehaviour
 {
-void OnTriggerEnter(Collider other)
-{
-    
-}
+    public GameObject angler;
+    public PlayableDirector timelineDirector;
+    void Start()
+    {
+        timelineDirector.stopped += OnTimelineStopped;
+    }
+    private void OnTimelineStopped(PlayableDirector director)
+    {
+
+        angler.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            angler.SetActive(true);
+            timelineDirector.Play();
+            StartCoroutine(ResetTriggerAfterTimeline());
+        }
+    }
+
+    private IEnumerator ResetTriggerAfterTimeline()
+    {
+        yield return new WaitForSeconds((float)timelineDirector.duration);
+        GetComponent<Collider>().enabled = true;
+    }
 }
